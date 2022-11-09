09a919d30ad6da8546b5d43095df72be3cd1f26b
dweisel <drewweisel@gmail.com>
2022-11-08 20:47:05 -0700

    fixed bugs

--- script.js
+++ script.js
@@ -13,10 +13,13 @@
 // After all cards have been played, display the score and declare the winner.
 // Write a Unit Test using Mocha and Chai for at least one of the functions you write.
 
+//creates a deck class with an empty array to pass in cards (suits + values)
 class Deck {
     constructor() {
         this.deck = [];
     }
+
+    // method that creates a 52 card deck
     createDeck(){
         const suits = ['♠', '♣', '♥', '♦']
 const values = [
@@ -50,7 +53,9 @@ const cardValueMap = {
     'K': 13,
     'A': 14,
 }
-
+// loop that pairs a suit with a value
+// each new pair is stored as a separate variable called newCard
+// all the newCard variables are pushed to the deck array
 for (let i = 0; i < suits.length; i++) {
     for (let x = 0; x < values.length; x++) {
         let newCard = { Value: values[x], Suit: suits[i]};
@@ -58,7 +63,7 @@ for (let i = 0; i < suits.length; i++) {
     }
 }
 }
-
+// method to shuffle the elements of the deck array
     shuffleDeck() {
         for (let i = this.deck.length - 1; i > 0; i--) {
             const newIndex = Math.floor(Math.random() * (i + 1))
@@ -70,22 +75,29 @@ for (let i = 0; i < suits.length; i++) {
     }
 }
 
+//creates an instance of deck array
 let myDeck = new Deck;
 myDeck.createDeck()
 myDeck.shuffleDeck()
 console.log(myDeck);
 
+//creates a variable that is the middle value of the myDeck array
 const deckMidPoint = Math.ceil(myDeck.deck.length / 2);
+
+//use splice function to cut from the first index in the myDeck array to the deckMidPoint value, creates variable playerOneHand
 const playerOneHand = myDeck.deck.splice(0, deckMidPoint);
+
+//use splice function to cut from the last index in the myDeck array to the deckMidPoint value, creates variable playerTwoHand
 const playerTwoHand = myDeck.deck.splice(-deckMidPoint);
 
 console.log(playerOneHand);
 console.log(playerTwoHand);
 
-
+//creates variables to hold score (cannot use const because values change)
 let playerOneScore = 0;
 let playerTwoScore = 0;
 
+//this function flips a card, or starts the game
 function flipCard() {
  
   for (let i = 0; i < 26 ; i++) {
@@ -106,6 +118,7 @@ function flipCard() {
   Final Scores: Player One: ${playerOneScore}, Player Two: ${playerTwoScore}`);  
 }
 
+// invokes the function to start the game
 flipCard();
 
 if(playerOneScore > playerTwoScore) {
